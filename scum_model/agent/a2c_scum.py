9d2cbdbbf3a093af25ae7a6f633874802563beab
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-11-02 18:35:20 +0000

    Add ELO rating through agent_evalaution

--- scum_model/agent/a2c_scum.py
+++ scum_model/agent/a2c_scum.py
@@ -3,7 +3,6 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-
 import numpy as np
 
 from agent.agent_pool import AgentPool
@@ -14,43 +13,56 @@ from torch.utils.tensorboard import SummaryWriter
 
 from tqdm import tqdm
 import argparse
-
 writer = SummaryWriter()
 
 class A2CScum:
-    def __init__(self, number_of_agents, load_checkpoints, episodes, callback, **kwargs):
+    def __init__(self, number_of_agents, agent_pool: AgentPool=None, callback=None, load_checkpoints=False, **kwargs):
         self.env = ScumEnv(number_players=number_of_agents)
-        self.agent_pool = AgentPool(number_of_agents, load_checkpoints, **kwargs)
+        self.agent_pool = AgentPool(number_of_agents, load_checkpoints, **kwargs) if agent_pool is None else agent_pool
         self.total_steps = 0
         self.ep_rewards = [[] for _ in range(number_of_agents)]
-        self.total_episodes = episodes
         self.last_mean_average_reward = [-np.inf for _ in range(5)]
         self.aggregate_states_every = C.AGGREGATE_STATS_EVERY
         self.callback = callback
-        self.eval_agent_pool = AgentPool(number_of_agents, load_checkpoints=False, load_eval=True, **kwargs)
     
-    def learn(self):
-        for episode in tqdm(range(1, self.total_episodes + 1), ascii=True, unit='episodes'):
+    def learn(self, total_episodes, eval_every: int = 5000):
+        from agent_evaluation.single_elo import AgentTestingSystem  # Move import here
+ 
+        agent_testing = AgentTestingSystem(load_path="objects/agent_testing_system.pkl")
+        
+        for episode in tqdm(range(1, total_episodes + 1), ascii=True, unit='episodes'):
             episode_rewards = self.run_episode()
         
             for i, reward in enumerate(episode_rewards):
                 self.ep_rewards[i].append(reward)
 
+            if episode % eval_every == 0:
+                ratings = []
+                for i in range(C.NUMBER_OF_AGENTS):
+                    agent = self.agent_pool.get_agent(i)
+                    results = agent_testing.test_model(agent, num_opponents=C.NUMBER_OF_AGENTS)
+                    print("Results of ELO are: ", results)
+
+                    rating = agent_testing.get_model_summary(model_id=agent.model.id)['rating']
+                    ratings.append(rating)
+                max_rating = np.array(ratings).max()
+                self.callback.last_mean_reward = max_rating
+                print("Max ELO score is: ", str(max_rating))
+                
+                agent_testing.save_system(path="objects/agent_testing_system.pkl")
+
+                
             if episode % self.aggregate_states_every == 0:
                 average_rewards = []
                 for i, average_reward in self.log_stats(episode):
                     average_rewards.append(average_reward)
-                    self.save_models(self.agent_pool, i)
                 
-                self.last_mean_average_reward = average_reward.copy()
+                self.last_mean_average_reward = average_reward
                 
                 self.agent_pool.update_order(average_rewards)
                 self.agent_pool.refresh_agents()
                 self.agent_pool.save_agents()
 
-                self.eval_agent_pool.set_agent(self.get_better_model())
-                self.callback.last_mean_reward = np.array(self.eval(self.callback.n_eval_episodes)).mean()
-            
             # Call the callback after each episode
             if self.callback is not None:
                 if not self.callback._on_step():
@@ -63,8 +75,6 @@ class A2CScum:
         
         return ep_rewards
 
-
-
     def run_episode(self, eval: bool=False):
         finish_agents = [False] * 5
         episode_rewards = [0] * 5
@@ -72,10 +82,9 @@ class A2CScum:
         print("Starting episode")
         print("_" * 100)
 
-        agent_pool = self.agent_pool if eval else self.eval_agent_pool
 
-        while np.array(finish_agents).sum() != agent_pool.number_of_agents:
-            agent = agent_pool.get_agent(self.env.player_turn)
+        while np.array(finish_agents).sum() != self.agent_pool.number_of_agents:
+            agent = self.agent_pool.get_agent(self.env.player_turn)
 
             state = self.env.get_cards_to_play()
             action, rw_decision = agent.decide_move(state)
@@ -95,12 +104,13 @@ class A2CScum:
 
             self.total_steps += 1
         
-        if eval == False:           
-            agent.train(self.total_steps)
+        if eval == False:
+            for i in range(self.agent_pool.number_of_agents):
+                agent = self.agent_pool.get_agent(i)
+                results = agent.train(self.total_steps)
+                print(results)
         
-        ## Only interested in the first agent
-        if eval:
-            return episode_rewards[0]
+
         
         return episode_rewards
 
@@ -115,8 +125,9 @@ class A2CScum:
             writer.flush()
             yield i, average_reward
 
-    def save_models(agent_pool: AgentPool, i: int) -> None:
-        agent_pool.get_agent(i).save_model(path=f"models/checkpoints/agent_{i+1}.pt")
+    # def save_models(self, i: int) -> None:
+    #     self.agent_pool.get_agent(i).save_model(path=f"models/checkpoints/agent_{i+1}.pt")
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the Scum Environment simulation")
