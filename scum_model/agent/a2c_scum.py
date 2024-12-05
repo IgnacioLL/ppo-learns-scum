0f6d1ab46e329493c658cbff8197b5d7e8f69bd7
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-12-05 12:20:49 +0000

    code refactor

--- scum_model/agent/a2c_scum.py
+++ scum_model/agent/a2c_scum.py
@@ -13,6 +13,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 from tqdm import tqdm
 import argparse
+
+from typing import List
+
 writer = SummaryWriter()
 
 class A2CScum:
@@ -21,52 +24,19 @@ class A2CScum:
         self.agent_pool = AgentPool(number_of_agents, load_checkpoints, **kwargs) if agent_pool is None else agent_pool
         self.total_steps = 0
         self.ep_rewards = [[] for _ in range(number_of_agents)]
-        self.last_mean_average_reward = [-np.inf for _ in range(5)]
-        self.aggregate_states_every = C.AGGREGATE_STATS_EVERY
+        self.aggregate_stats_every = C.AGGREGATE_STATS_EVERY
         self.callback = callback
     
-    def learn(self, total_episodes, eval_every: int = 5000):
-        from agent_evaluation.single_elo import AgentTestingSystem  # Move import here
- 
-        agent_testing = AgentTestingSystem(load_path="objects/agent_testing_system.pkl")
-        
+    def learn(self, total_episodes):
         for episode in tqdm(range(1, total_episodes + 1), ascii=True, unit='episodes'):
-            episode_rewards = self.run_episode()
-        
-            for i, reward in enumerate(episode_rewards):
-                self.ep_rewards[i].append(reward)
+            episode_rewards = self.run_episode(episode)
 
-            if episode % eval_every == 0:
-                ratings = []
-                for i in range(C.NUMBER_OF_AGENTS):
-                    agent = self.agent_pool.get_agent(i)
-                    results = agent_testing.test_model(agent, num_opponents=C.NUMBER_OF_AGENTS)
-                    print("Results of ELO are: ", results)
+            self.append_rewards_to_historic_record(episode_rewards)
 
-                    rating = agent_testing.get_model_summary(model_id=agent.model.id)['rating']
-                    ratings.append(rating)
-                max_rating = np.array(ratings).max()
-                self.callback.last_mean_reward = max_rating
-                print("Max ELO score is: ", str(max_rating))
-                
-                agent_testing.save_system(path="objects/agent_testing_system.pkl")
+            if episode % self.aggregate_stats_every == 0:
+                average_rewards = self.get_average_reward_last_n_episodes(episode)
+                # self.agent_pool.swap_worst_models_for_best_ones(average_rewards)
 
-                
-            if episode % self.aggregate_states_every == 0:
-                average_rewards = []
-                for i, average_reward in self.log_stats(episode):
-                    average_rewards.append(average_reward)
-                
-                self.last_mean_average_reward = average_reward
-                
-                self.agent_pool.update_order(average_rewards)
-                self.agent_pool.refresh_agents()
-                self.agent_pool.save_agents()
-
-            # Call the callback after each episode
-            if self.callback is not None:
-                if not self.callback._on_step():
-                    break  # Stop training if the callback returns False
 
     def eval(self, episodes):
         ep_rewards = []
@@ -75,59 +45,69 @@ class A2CScum:
         
         return ep_rewards
 
-    def run_episode(self, eval: bool=False):
-        finish_agents = [False] * 5
+    def run_episode(self, episode, eval: bool=False):
+        done_agents = [False] * 5
         episode_rewards = [0] * 5
         self.env.reset()
-        print("Starting episode")
-        print("_" * 100)
 
-
-        while np.array(finish_agents).sum() != self.agent_pool.number_of_agents:
+        while np.array(done_agents).sum() != self.agent_pool.number_of_agents:
             agent = self.agent_pool.get_agent(self.env.player_turn)
 
             state = self.env.get_cards_to_play()
-            action, rw_decision = agent.decide_move(state)
-            current_state, new_state, reward, finish, agent_number = self.env.step(action, state)
+            action = agent.decide_move(state)
+            current_state, new_state, reward, done, agent_number = self.env.step(action, state)
 
-            reward = reward + rw_decision
-            finish_agents[agent_number] = finish
+            done_agents[agent_number] = done
             episode_rewards[agent_number] += reward
 
-            agent.memory.append((current_state, reward, new_state))
+            agent.save_in_buffer(current_state, reward, new_state)
             
-            if finish:
-                current_state, new_state, reward = self.env.get_stats_after_finish(agent_number=agent_number)
+            if done:
+                current_state, new_state, reward = self.env.get_stats_after_done(agent_number=agent_number)
                 episode_rewards[agent_number] += reward
-                agent.memory.append((current_state, reward, new_state))
-                finish_agents[agent_number] = finish
+                agent.save_in_buffer(current_state, reward, new_state)
+                done_agents[agent_number] = done
 
             self.total_steps += 1
         
         if eval == False:
-            for i in range(self.agent_pool.number_of_agents):
-                agent = self.agent_pool.get_agent(i)
-                results = agent.train(self.total_steps)
-                print(results)
-        
+            training_performance_stats = self.train_models()
+            self.flush_performance_stats_tensorboard(training_performance_stats, episode)
 
-        
         return episode_rewards
 
-    def log_stats(self, episode):
-        for i in range(self.agent_pool.number_of_agents):
-            recent_rewards = self.ep_rewards[i][-C.AGGREGATE_STATS_EVERY:]
-            average_reward = sum(recent_rewards) / len(recent_rewards)
-            min_reward = min(recent_rewards)
-            max_reward = max(recent_rewards)
-            print(f"Agent {i+1}: Avg: {average_reward:.2f}, Min: {min_reward:.2f}, Max: {max_reward:.2f} and current learning rate {self.agent_pool.get_agent(i).current_learning_rate:.6f}")
-            writer.add_scalar(f"Reward/Agent {i+1}/Avg Reward", average_reward, episode)
-            writer.flush()
-            yield i, average_reward
+    def train_models(self):
+        performance_stats = {}
+        for agent_number in range(self.agent_pool.number_of_agents):
+            agent = self.agent_pool.get_agent(agent_number)
+            performance_stats[agent_number] = agent.train(self.total_steps)
+        return performance_stats
 
-    # def save_models(self, i: int) -> None:
-    #     self.agent_pool.get_agent(i).save_model(path=f"models/checkpoints/agent_{i+1}.pt")
-        
+    def append_rewards_to_historic_record(self, episode_rewards: List[float]):
+        for agent_number, reward in enumerate(episode_rewards):
+            self.ep_rewards[agent_number].append(reward)
+
+    def _compute_average_reward_last_n_episodes(self, n_episodes):
+        agent_rewards = []
+        for i in range(self.agent_pool.number_of_agents):
+            recent_rewards = self.ep_rewards[i][-n_episodes:]
+            average_reward = sum(recent_rewards) / len(recent_rewards)
+            agent_rewards.append(average_reward)
+        return agent_rewards
+
+    def get_average_reward_last_n_episodes(self, n_episodes=C.AGGREGATE_STATS_EVERY) -> List[float]:
+        return self._compute_average_reward_last_n_episodes(n_episodes)
+
+    def flush_performance_stats_tensorboard(self, performance_stats: dict, episode: int) -> None:
+        for agent_number in performance_stats:
+            for stat in performance_stats[agent_number]:  
+                writer.add_scalar(f"{stat}/Agent {int(agent_number)+1}", 
+                                  performance_stats[agent_number][stat], episode)
+        writer.flush()
+
+    def flush_average_reward_to_tensorboard(self, average_rewards: list, episode: int):
+        average_rewards_format = {agent_number : {"avg_reward": avg_reward} for agent_number, avg_reward in enumerate(average_rewards)}
+        self.flush_performance_stats_tensorboard(average_rewards_format, episode)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the Scum Environment simulation")
