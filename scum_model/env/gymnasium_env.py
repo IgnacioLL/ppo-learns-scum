807bbc46e07640b5809800a2e709e6e12c313861
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-12-27 17:39:53 +0100

    Reorder code and change state space

--- scum_model/env/gymnasium_env.py
+++ scum_model/env/gymnasium_env.py
@@ -13,7 +13,7 @@ from typing import List, Tuple
 from math import ceil
 import random
 import collections
-from utils.utils import move_to_last_position, convert_to_binary_tensor
+from utils.utils import move_to_last_position, convert_to_binary_tensor, compact_form_of_states
 
 class ScumEnv(gym.Env):
     def __init__(self, number_players):
@@ -37,7 +37,6 @@ class ScumEnv(gym.Env):
         self.previous_done = [False] * self.number_players
 
         self.cards_thrown = [0] * (C.NUMBER_OF_CARDS_PER_SUIT + 1)
-
         
         # Define action and observation space
         # For example, if you have discrete actions, use spaces.Discrete
@@ -51,23 +50,22 @@ class ScumEnv(gym.Env):
 
     def reset(self) -> None:
         self.__init__(self.number_players)
+
+    def render(self, mode='human'):
+        """Render the environment (optional)"""
+        print(f"Current State: {self.cards}")
+
     
     def step(self, action: int, current_state: torch.tensor):
-        ## This will be the variables returned to compute the reward.
-        ## We will use the past events to be able to compute the new states.
         agent_number = self.player_turn
 
-        previous_state = self.state[agent_number]
-        previous_reward = self.previous_reward[agent_number]
-        new_state = current_state ## this is the new state
-
         # Update previous state to the new state
-        self.state[agent_number] = new_state
+        self.state[agent_number] = current_state
 
         n_cards, card_number = self._decode_action(action)
 
         if self._is_pass_action(n_cards):
-            return self._handle_pass_action(previous_state, new_state, previous_reward, False, agent_number)
+            return self._handle_pass_action(current_state, agent_number)
 
         skip = self._is_skip_move(card_number)
 
@@ -76,7 +74,7 @@ class ScumEnv(gym.Env):
         
         ## Check if the player has done the game and compute the reward
         done, done_reward = self._check_player_done()
-        cards_reward = self._calculate_cards_reward(n_cards, done)
+        cards_reward = self._calculate_cards_reward(action, done)
         total_reward = cards_reward + done_reward
 
         ## Update the self.previous_reward and self.previous_done
@@ -85,54 +83,32 @@ class ScumEnv(gym.Env):
         ## This changes self.player_turn
         self._update_player_turn(skip)
 
-        return previous_state, new_state, previous_reward, done, agent_number
+        return current_state, total_reward, done, agent_number
     
-    def render(self, mode='human'):
-        """Render the environment (optional)"""
-        print(f"Current State: {self.cards}")
+    def _decode_action(self, action: int) -> Tuple[int, int]:
+        n_cards = action // (C.NUMBER_OF_CARDS_PER_SUIT + 1)
+        card_number = action % (C.NUMBER_OF_CARDS_PER_SUIT + 1)
+        if card_number == 0:
+            card_number = C.NUMBER_OF_CARDS_PER_SUIT + 1
+        return n_cards, card_number
 
-    def _compute_cards_x_player(self, cards: List[int]) -> int:
-        number_of_cards = len(cards)
-        number_of_cards_x_person = ceil((C.NUMBER_OF_SUITS*C.NUMBER_OF_CARDS_PER_SUIT)/self.number_players)
-        return number_of_cards/number_of_cards_x_person
+    def _is_pass_action(self, n_cards: int) -> bool:
+        return n_cards == 4
     
-    def get_number_cards_x_person(self) -> List[int]:
-        return [self._compute_cards_x_player(cards_player[0]) for cards_player in self.cards]
+    def _handle_pass_action(self, current_state, agent_number):
+        self._handle_unable_to_play()
+        return current_state, C.REWARD_PASS, False, agent_number
 
-    def _get_deck(self) -> List[int]:
-        deck = [rank for _ in range(C.NUMBER_OF_SUITS) for rank in range(1, C.NUMBER_OF_CARDS_PER_SUIT + 1)]
-        deck.remove(C.NUMBER_OF_CARDS_PER_SUIT)
-        deck.append(C.NUMBER_OF_CARDS_PER_SUIT + 1)  # 2 of hearts
-        return deck
-
-    def _deal_cards(self) -> List[List[List[int]]]:
-        deck = self._get_deck()
-        sampled_data = random.sample(deck, C.NUMBER_OF_CARDS)
-        
-        cards_per_player = C.NUMBER_OF_CARDS // self.number_players
-        remainder = C.NUMBER_OF_CARDS % self.number_players
-        
-        cards_of_players = []
-        for i in range(self.number_players):
-            start = i * cards_per_player + min(i, remainder)
-            end = start + cards_per_player + (1 if i < remainder else 0)
-            player_cards = sorted(sampled_data[start:end])
-            cards_of_players.append(self._get_combinations(player_cards))
-
-        return cards_of_players
-
-    @staticmethod
-    def _extract_higher_order(cards: List[int], number_of_repetitions: int) -> List[int]:
-        return list(set(item for item, count in collections.Counter(cards).items() if count >= number_of_repetitions))
-
-    @classmethod
-    def _get_combinations(cls, cards: List[int]) -> List[List[int]]:
-        return [
-            cards,
-            cls._extract_higher_order(cards, 2),
-            cls._extract_higher_order(cards, 3),
-            cls._extract_higher_order(cards, 4)
-        ]
+    def _handle_unable_to_play(self) -> None:
+        self.players_in_round[self.player_turn] = False
+        self.n_players_in_round -= 1
+        self._update_player_turn()
+        self._check_players_playing()
+    
+    def _update_player_turn(self, skip: bool = False) -> None:
+        turns_to_skip = 2 if skip else 1
+        for _ in range(turns_to_skip):
+            self.player_turn = self._next_turn()
 
     def _next_turn(self) -> int:
         next_player = (self.player_turn + 1) % self.number_players
@@ -142,10 +118,28 @@ class ScumEnv(gym.Env):
                 break
         return next_player
 
-    def _update_player_turn(self, skip: bool = False) -> None:
-        turns_to_skip = 2 if skip else 1
-        for _ in range(turns_to_skip):
-            self.player_turn = self._next_turn()
+
+    def _check_players_playing(self) -> None:
+        if self.n_players_in_round <= 1:
+            if self.last_player != -1:  
+                self.player_turn = self.last_player
+            self._reinitialize_round()
+
+    def _reinitialize_round(self) -> None:
+        self.last_player = -1
+        self.last_move = None
+        self.players_in_round = self.players_in_game.copy()
+        self.n_players_in_round = sum(self.players_in_round)
+
+    def _is_skip_move(self, card_number: int) -> bool:
+        return self.last_move is not None and self.last_move[0] == card_number
+    
+
+    def _update_game_state(self, card_number: int, n_cards: int):
+        self._update_player_cards(card_number, n_cards)
+        self.last_move = [card_number, n_cards]
+        self.last_player = self.player_turn
+
 
     def _update_player_cards(self, card_number: int, n_cards: int) -> None:
         if card_number == C.NUMBER_OF_CARDS_PER_SUIT + 1:  # two of hearts
@@ -156,17 +150,6 @@ class ScumEnv(gym.Env):
         self.cards[self.player_turn] = self._get_combinations(self.cards[self.player_turn][0])
         self.cards_thrown[card_number - 1] = (n_cards + 1) * (1/C.NUMBER_OF_SUITS)
 
-    def _reinitialize_round(self) -> None:
-        self.last_player = -1
-        self.last_move = None
-        self.players_in_round = self.players_in_game.copy()
-        self.n_players_in_round = sum(self.players_in_round)
-
-    def _check_players_playing(self) -> None:
-        if self.n_players_in_round <= 1:
-            if self.last_player != -1:  
-                self.player_turn = self.last_player
-            self._reinitialize_round()
 
     def _check_player_done(self) -> Tuple[bool, int]:
         if not self.cards[self.player_turn][0]:
@@ -188,18 +171,46 @@ class ScumEnv(gym.Env):
             self._reinitialize_round()
             return True, done_reward
         return False, 0
+    
+    def _calculate_cards_reward(self, action: int, done: bool) -> int:
+        n_cards, card_number = self._decode_action(action)
+        if card_number == 14:
+            return C.REWARD_CARD + C.REWARD_EMPTY_HAND * int(done)
+        else:
+            return C.REWARD_CARD * (n_cards + 1) +  C.REWARD_EMPTY_HAND * int(done)
+    
+    def _update_previous_state(self, total_reward: int, done: bool):
+        self.previous_reward[self.player_turn] = total_reward
+        self.previous_done[self.player_turn] = done
 
-    def convert_to_binary_player_turn_cards(self) -> torch.tensor:
-        return convert_to_binary_tensor(self.cards[self.player_turn], pass_option=True)
+    
+    def get_state(self) -> torch.tensor:
+        if sum(self.players_in_game) == 0:
+            return
 
-    def _get_cards_to_play(self) -> np.array:
+        if self.last_player == self.player_turn:
+            self._reinitialize_round()
+            return self.get_state()
+
+        cards = convert_to_binary_tensor(self.cards[self.player_turn], pass_option=True)
+
+        players_info = self.get_number_cards_x_person()
+        players_info = move_to_last_position(players_info, self.player_turn)
+        cards_thrown = self.get_cards_thrown()
+
+        compact_action_space = compact_form_of_states(self.get_action_space())
+
+        state = torch.cat([cards, compact_action_space.to(C.DEVICE), torch.tensor(players_info, device=C.DEVICE), torch.tensor(cards_thrown, device=C.DEVICE)])
+        
+        return state
+
+    def get_action_space(self) -> np.array:
         if self.last_move is None:
             return self._get_cards_to_play_init()
         else:
             return self._get_cards_to_play_followup()
 
     def _get_cards_to_play_init(self) -> torch.tensor:
-        available_combinations = [i for i in range(C.NUMBER_OF_SUITS) if self.cards[self.player_turn][i]]
         return convert_to_binary_tensor(self.cards[self.player_turn], pass_option=False) ## the pass action which is not available in the first move
 
     def _get_cards_to_play_followup(self) -> torch.tensor:
@@ -215,75 +226,61 @@ class ScumEnv(gym.Env):
             cards = [cards for cards in possibilities if cards >= self.last_move[0]] + two_of_hearts
         cards = [cards if index == n_cards else [] for index in range(4)]
         return convert_to_binary_tensor(cards, pass_option=True) ## add the pass action
+
+    def get_number_cards_x_person(self) -> List[int]:
+        return [self._compute_cards_x_player(cards_player[0]) for cards_player in self.cards]
+
+    def _compute_cards_x_player(self, cards: List[int]) -> int:
+        number_of_cards = len(cards)
+        number_of_cards_x_person = ceil((C.NUMBER_OF_SUITS*C.NUMBER_OF_CARDS_PER_SUIT)/self.number_players)
+        return number_of_cards/number_of_cards_x_person
     
+
     def get_cards_thrown(self):
         return self.cards_thrown
+
+
+    def _deal_cards(self) -> List[List[List[int]]]:
+        deck = self._get_deck()
+        sampled_data = random.sample(deck, C.NUMBER_OF_CARDS)
         
-    def get_cards_to_play(self) -> torch.tensor:
-        if sum(self.players_in_game) == 0:
-            return
-
-        if self.last_player == self.player_turn:
-            self._reinitialize_round()
-            return self.get_cards_to_play()
-
-        action_space: torch.tensor = self._get_cards_to_play()
-
-        players_info = self.get_number_cards_x_person()
-        players_info = move_to_last_position(players_info, self.player_turn)
-        cards_thrown = self.get_cards_thrown()
-
-        state = torch.cat([action_space, torch.tensor(players_info, device=C.DEVICE), torch.tensor(cards_thrown, device=C.DEVICE)])
+        cards_per_player = C.NUMBER_OF_CARDS // self.number_players
+        remainder = C.NUMBER_OF_CARDS % self.number_players
         
-        return state
+        cards_of_players = []
+        for i in range(self.number_players):
+            start = i * cards_per_player + min(i, remainder)
+            end = start + cards_per_player + (1 if i < remainder else 0)
+            player_cards = sorted(sampled_data[start:end])
+            cards_of_players.append(self._get_combinations(player_cards))
 
+        return cards_of_players
 
-    def _handle_unable_to_play(self) -> None:
-        self.players_in_round[self.player_turn] = False
-        self.n_players_in_round -= 1
-        self._update_player_turn()
-        self._check_players_playing()
-
-
+    def _get_deck(self) -> List[int]:
+        deck = [rank for _ in range(C.NUMBER_OF_SUITS) for rank in range(1, C.NUMBER_OF_CARDS_PER_SUIT + 1)]
+        deck.remove(C.NUMBER_OF_CARDS_PER_SUIT)
+        deck.append(C.NUMBER_OF_CARDS_PER_SUIT + 1)  # 2 of hearts
+        return deck
     
+
+    @classmethod
+    def _get_combinations(cls, cards: List[int]) -> List[List[int]]:
+        return [
+            cards,
+            cls._extract_higher_order(cards, 2),
+            cls._extract_higher_order(cards, 3),
+            cls._extract_higher_order(cards, 4)
+        ]
+
+
+    @staticmethod
+    def _extract_higher_order(cards: List[int], number_of_repetitions: int) -> List[int]:
+        return list(set(item for item, count in collections.Counter(cards).items() if count >= number_of_repetitions))
+
     def get_stats_after_done(self, agent_number: int) -> Tuple[int, int, int]:
         current_state = self.state[agent_number]
-        new_state = torch.zeros((C.NUMBER_OF_POSSIBLE_STATES) + self.number_players + C.NUMBER_OF_CARDS_PER_SUIT+1)
         reward = self.previous_reward[agent_number]
-        return current_state, new_state, reward
-
-    def _decode_action(self, action: int) -> Tuple[int, int]:
-        n_cards = action // (C.NUMBER_OF_CARDS_PER_SUIT + 1)
-        card_number = action % (C.NUMBER_OF_CARDS_PER_SUIT + 1)
-        if card_number == 0:
-            card_number = C.NUMBER_OF_CARDS_PER_SUIT + 1
-        return n_cards, card_number
-
-    def _is_pass_action(self, n_cards: int) -> bool:
-        return n_cards == 4
-
-    def _handle_pass_action(self, previous_state, new_state, previous_reward, previous_done, agent_number):
-        self.previous_reward[agent_number] = C.REWARD_PASS
-        self._handle_unable_to_play()
-        return previous_state, new_state, previous_reward, previous_done, agent_number
-
-    def _is_skip_move(self, card_number: int) -> bool:
-        return self.last_move is not None and self.last_move[0] == card_number
-
-    def _update_game_state(self, card_number: int, n_cards: int):
-        self._update_player_cards(card_number, n_cards)
-        self.last_move = [card_number, n_cards]
-        self.last_player = self.player_turn
-
-    def _calculate_cards_reward(self, n_cards: int, done: bool) -> int:
-        cards_reward = C.REWARD_CARD * (n_cards + 1)
-        if done:
-            cards_reward += C.REWARD_EMPTY_HAND
-        return cards_reward
-
-    def _update_previous_state(self, total_reward: int, done: bool):
-        self.previous_reward[self.player_turn] = total_reward
-        self.previous_done[self.player_turn] = done
+        return current_state, reward
 
     def _print_game_state(self) -> None:
         if self.last_move is not None:
